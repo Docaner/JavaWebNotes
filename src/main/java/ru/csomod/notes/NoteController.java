829f1f6613feee97c6003fe512be4fd42cb76e55
Docaner <danil241202@gmail.com>
2023-10-06 17:35:06 +0300

    MVC ModelTest

--- src/main/java/ru/csomod/notes/NoteController.java
+++ src/main/java/ru/csomod/notes/NoteController.java
@@ -1,13 +1,31 @@
 package ru.csomod.notes;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/note")
 public class NoteController {
 
-    @GetMapping("/note")
-    public String sayHello(){
-        return "test";
+    @GetMapping("/post")
+    public String postNote(@RequestParam(required = false) String header,
+                          @RequestParam(required = false) String content){
+        //System.out.println("Header: " + header + "\nContent: " + content);
+        return "note/post";
+    }
+
+    @GetMapping("/get")
+    public String getNote(Model model){
+        model.addAttribute("header", "Заголовок 1");
+        model.addAttribute("content", "Контент 2");
+        return "note/get";
+    }
+
+    @GetMapping("/list")
+    public String getNotes() {
+        return "note/post";
     }
 }
